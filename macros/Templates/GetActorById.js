39de65f5dddf35df86f5cd73deb9e710b4da9548
Ryan Warren <ryanwarrendev@gmail.com>
2024-11-08 20:00:01 -0600

    update macros

--- macros/Templates/GetActorById.js
+++ macros/Templates/GetActorById.js
@@ -1,3 +1,8 @@
-const actorId = 'ECbH2yr4BL2jmEeP';
+const actorId = 'ez4qAGiUi2DzjmH5';
 let controlledActor = game.actors.get(actorId);
-//console.log(controlledActor);
+console.log(controlledActor);
+
+
+
+//While selecting a token, execute the macro. Then, look at the console. //This will tell you all the data that is available to you when writing //a formula.
+console.log(controlledActor.getRollData())
